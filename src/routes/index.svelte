8c92d460266c3e1be028370b534110b05471f066
Aly Ibrahim <aly.ibrahim@gmail.com>
2022-07-28 20:14:43 +0000

    Fix: Subscribe Button and SW

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,19 +10,20 @@ import InputForm from '$lib/InputForm.svelte'
 //     console.log(sw)
 // })
 
-onMount(async () => { handleSubscription() })
+// onMount(async () => { handleSubscription() })
 
 const handleSubscription = async () =>{
     let sw = await navigator.serviceWorker.ready
     let push = await sw.pushManager.subscribe({
         userVisibleOnly: true,
-        applicationServerKey: 'BO5vVQK3W3oq4Ll6q9F0QPGaa74poJdy6tW8vgjnpADkAVL7tSOpn23GCa22EjoWBiouk6GzKc4Vhg6yv78owJk'
+        applicationServerKey: 'BNdEaJB-O7dfF66zyezdC0kyauy6GJQKhMvwrKEO5ATrPRC8v7F7WVGOLenGlPdKfrPfJq3GTpdlpmavc7dHbYA'
     })
     console.log(JSON.stringify(push))
 }
 
 </script>
 <h1>Welcome to SvelteKit</h1>
+<button on:click="{handleSubscription}">Subscribe to Notifications</button>
 <InputForm />
 <!-- <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
 <!-- <h1>Hello {import.meta.env.VITE_NAME}</h1> -->
