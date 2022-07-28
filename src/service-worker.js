c3489bbf6c2c33af89f383d203a15c233708c599
Aly Ibrahim <aly.ibrahim@gmail.com>
2022-07-28 18:30:16 +0000

    Fix: SW showNotification

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,3 +1,15 @@
-self.addEventListener('push', () => {
-    self.registration.sendNotification('Hello From SW !!')
+self.addEventListener('push', (event) => {
+
+    let data = event.data.json();
+    // const image = 'https://cdn.glitch.com/614286c9-b4fc-4303-a6a9-a4cef0601b74%2Flogo.png?v=1605150951230';
+    const options = {
+      body: data.options.body,
+    //   icon: image
+    }
+    self.registration.showNotification(
+      data.title, 
+      options
+    );
+    //self.registration.sendNotification('Hello From SW !!')
+    //self.registration.sendNotification('Hello From SW !!')
 })
